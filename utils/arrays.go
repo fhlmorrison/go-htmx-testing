464148ac68c01393af5ae51d019e706881f4790e
Fraser Morrison <frazemor@hotmail.com>
2024-01-28 23:58:24 -0500

    Added comments to array functions

--- utils/arrays.go
+++ utils/arrays.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// Remove an element from a slice by index (not in place)
 func Remove[T any](slice []T, s int) []T {
 	if s >= len(slice) {
 		fmt.Println("Index out of bounds", s, "for slice of length", len(slice), ".")
@@ -12,6 +13,7 @@ func Remove[T any](slice []T, s int) []T {
 	return append(slice[:s], slice[s+1:]...)
 }
 
+// Remove an element from a slice by value
 func RemoveElement[T comparable](slice []T, elem T) []T {
 	for i, e := range slice {
 		if e == elem {
@@ -21,6 +23,7 @@ func RemoveElement[T comparable](slice []T, elem T) []T {
 	return slice
 }
 
+// Return slice with elements filtered in by predicate
 func Filter[T any](slice []T, f func(T) bool) []T {
 	var filtered []T = make([]T, len(slice))
 	n := 0
@@ -33,6 +36,7 @@ func Filter[T any](slice []T, f func(T) bool) []T {
 	return filtered[:n]
 }
 
+// Return slice with elements mapped by function
 func Map[T any, U any](slice []T, f func(T) U) []U {
 	var mapped []U = make([]U, len(slice))
 	for i, elem := range slice {
@@ -41,6 +45,7 @@ func Map[T any, U any](slice []T, f func(T) U) []U {
 	return mapped
 }
 
+// Return reduced value of slice by function
 func Reduce[T any, U any](slice []T, f func(U, T) U, init U) U {
 	var reduced U = init
 	for _, elem := range slice {
@@ -49,6 +54,7 @@ func Reduce[T any, U any](slice []T, f func(U, T) U, init U) U {
 	return reduced
 }
 
+// Return true if any element in slice satisfies predicate
 func Any[T any](slice []T, f func(T) bool) bool {
 	for _, elem := range slice {
 		if f(elem) {
@@ -58,6 +64,7 @@ func Any[T any](slice []T, f func(T) bool) bool {
 	return false
 }
 
+// Return true if all elements in slice satisfy predicate
 func All[T any](slice []T, f func(T) bool) bool {
 	for _, elem := range slice {
 		if !f(elem) {
