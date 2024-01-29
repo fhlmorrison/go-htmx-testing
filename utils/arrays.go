92cf93d6e454a2b5f8f742405da6b8a9cf647a36
Fraser Morrison <frazemor@hotmail.com>
2024-01-28 23:32:11 -0500

    Array Functions (Filter, Map, Reduce)

--- utils/arrays.go
+++ utils/arrays.go
@@ -1,6 +1,8 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Remove[T any](slice []T, s int) []T {
 	if s >= len(slice) {
@@ -8,4 +10,50 @@ func Remove[T any](slice []T, s int) []T {
 		return slice
 	}
 	return append(slice[:s], slice[s+1:]...)
+}
+
+func Filter[T any](slice []T, f func(T) bool) []T {
+	var filtered []T = make([]T, len(slice))
+	n := 0
+	for _, elem := range slice {
+		if f(elem) {
+			filtered[n] = elem
+			n++
+		}
+	}
+	return filtered[:n]
+}
+
+func Map[T any, U any](slice []T, f func(T) U) []U {
+	var mapped []U = make([]U, len(slice))
+	for i, elem := range slice {
+		mapped[i] = f(elem)
+	}
+	return mapped
+}
+
+func Reduce[T any, U any](slice []T, f func(U, T) U, init U) U {
+	var reduced U = init
+	for _, elem := range slice {
+		reduced = f(reduced, elem)
+	}
+	return reduced
+}
+
+func Any[T any](slice []T, f func(T) bool) bool {
+	for _, elem := range slice {
+		if f(elem) {
+			return true
+		}
+	}
+	return false
+}
+
+func All[T any](slice []T, f func(T) bool) bool {
+	for _, elem := range slice {
+		if !f(elem) {
+			return false
+		}
+	}
+	return true
 }
